dae252f574dc1eb4ca59f962c025bfcc21a84ad9
Justedlev <edyardlyk@gmail.com>
2024-09-30 15:00:02 +0300

    fix: remove unused imports

--- src/main/java/io/justedlev/msrvs/cinfigcenter/ConfigCenterApplication.java
+++ src/main/java/io/justedlev/msrvs/cinfigcenter/ConfigCenterApplication.java
@@ -4,7 +4,6 @@ import io.justedlev.msrvs.cinfigcenter.configuration.SecurityProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
