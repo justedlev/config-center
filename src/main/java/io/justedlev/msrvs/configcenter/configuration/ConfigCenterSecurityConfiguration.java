c08afdb0824b8eb8cb2f90a5b5614572fb94816e
Edward L. <edward.lukashevits@gmail.com>
2025-12-20 12:51:16 +0200

    feat: update security configuration

--- src/main/java/io/justedlev/msrvs/configcenter/configuration/ConfigCenterSecurityConfiguration.java
+++ src/main/java/io/justedlev/msrvs/configcenter/configuration/ConfigCenterSecurityConfiguration.java
@@ -2,12 +2,16 @@ package io.justedlev.msrvs.configcenter.configuration;
 
 import io.justedlev.msrvs.configcenter.configuration.properties.ConfigCenterConfigurationProperties;
 import io.justedlev.msrvs.configcenter.configuration.properties.ConfigCenterSecurityConfigurationProperties;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.oauth2.client.ConditionalOnOAuth2ClientRegistrationProperties;
+import lombok.Setter;
+import org.jspecify.annotations.NonNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBooleanProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.lang.NonNull;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
@@ -15,7 +19,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@ConditionalOnOAuth2ClientRegistrationProperties
+@ConditionalOnBooleanProperty(prefix = "spring.security.oauth2", value = "enabled")
 @EnableConfigurationProperties({
         ConfigCenterConfigurationProperties.class,
         ConfigCenterSecurityConfigurationProperties.class,
@@ -24,8 +28,14 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ConfigCenterSecurityConfiguration {
     private final ConfigCenterSecurityConfigurationProperties properties;
 
+    @Getter
+    @Setter(onMethod_ = @Value("${spring.security.oauth2.enabled:true}"))
+    private boolean enabled;
+
+
     @Bean
-    public SecurityFilterChain securityFilterChain(@NonNull HttpSecurity httpSecurity) throws Exception {
+    @ConditionalOnMissingBean
+    public SecurityFilterChain securityFilterChain(@NonNull HttpSecurity httpSecurity) {
         return httpSecurity
                 .csrf(CsrfConfigurer::disable)
                 .sessionManagement(customizer ->
